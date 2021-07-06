aa013f9e95bd47aeefaf861a2ca6a2560744a575
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-06 19:31:44 +0900

    private 속성 사용하기

---
+++ 혼공스/마지막파트/클래스/9-2-6.js
@@ -0,0 +1,24 @@
+// 정사각형 클래스
+class Square {
+  #length; // 이 위치에 해당 속성을 private 속성으로 사용하겠다고 선언
+
+  constructor(length) {
+    if (length <= 0) {
+      throw "길이는 0 보다 커야 합니다.";
+    }
+
+    this.#length = length;
+  }
+
+  getPerimeter() {
+    return 4 * this.#length;
+  }
+  getArea() {
+    return this.#length * this.#length;
+  }
+}
+
+// 클래스 사용
+const square = new Square(10);
+console.log(`정사각형 둘레: ${square.getPerimeter()}`);
+console.log(`정사각형 넓이: ${square.getArea()}`);
