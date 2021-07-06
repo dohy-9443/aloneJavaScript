c8c6bde53b4674d330f8b9c74697021dd26f381c
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-06 19:32:42 +0900

    static 키워드 사용하기

---
+++ 혼공스/마지막파트/클래스/9-2-11.js
@@ -0,0 +1,53 @@
+class Square {
+  #length;
+  static #counter = 0; // private 특성과 static 특성은 한꺼번에 적용할 수도 있다.
+  static get counter() {
+    return Square.#counter;
+  }
+
+  constructor(length) {
+    this.length = length;
+    Square.#counter += 1;
+  }
+
+  static perimeterOf(length) {
+    return length * 4;
+  }
+
+  static areaOf(length) {
+    return length * length;
+  }
+
+  get length() {
+    return this.#length;
+  }
+
+  get perimeter() {
+    return this.#length * 4;
+  }
+
+  get area() {
+    return this.#length * this.#length;
+  }
+
+  set length(length) {
+    if (length <= 0) {
+      throw `길이는 0보다 커야 합니다.`;
+    }
+    this.#length = length;
+  }
+}
+
+// static 속성 사용
+const squareA = new Square(10);
+const squareB = new Square(20);
+const squareC = new Square(30);
+console.log(`지금까지 생성된 Square 인스턴스는 ${Square.counter}개 입니다.`);
+
+// static 메소드 사용하기
+console.log(
+  `한 변의 길이가 20인 정사각형의 둘레는 ${Square.perimeterOf(20)} 입니다.`
+);
+console.log(
+  `한 변의 길이가 30인 정사각형의 넓이는 ${Square.areaOf(30)} 입니다.`
+);
