dca320cee65a0f490c56385eb1e0deb65aeddf38
dohy-9443 <dong_hyun95@hanmail.net>
2021-07-06 19:33:28 +0900

    toString() 메소드 오버라이드하기

---
+++ 혼공스/마지막파트/클래스/9-2-15.js
@@ -0,0 +1,13 @@
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  toString() {
+    return `이름 ${this.name}\n나이: ${this.age}살`;
+  }
+}
+
+const pet = new Pet("구름", 6);
+console.log(pet + "");
